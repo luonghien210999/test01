ae7571b42f8e13c7ffdb528115e9ce10105f4905
Baba <hienb1809458@student.ctu.edu.vn>
2021-11-11 16:10:23 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+    <div class="navbar-nav">
+      <a class="nav-item nav-link active" href="index.php">Home</a>
+      <a class="nav-item nav-link" href="createdb.php">Create Database</a>
+      <a class="nav-item nav-link" href="add.php">Add</a>
+      <a class="nav-item nav-link" href="list.php">List</a>
+    </div>
+  </div>
+</nav>
+    <h1>Lương Minh Hiển</h1>
+    <h2>B1809458</h2>
+
+</body>
+</html>
